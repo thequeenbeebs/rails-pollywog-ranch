875f8c6be96e7e14153c38cbc499b79302362afa
Blaire Baker <bakerblaire@gmail.com>
2021-01-03 14:42:09 -0600

    done

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get '/tadpoles/:id/destroy', to: 'tadpoles#destroy'
 
   # code your post to  '/tadpoles/:id/metamorphose' here
-  # it should direct to the tadpoles controller, the metamorphose action
+  post '/tadpoles/:id/metamorphose', to: 'tadpoles#metamorphose', as: "metamorphose"
 
   # resources
   resources :ponds
--- db/schema.rb
+++ db/schema.rb
@@ -19,14 +19,15 @@ ActiveRecord::Schema.define(version: 20140922151507) do
   end
 
   create_table "ponds", force: :cascade do |t|
-    t.string "water_type"
     t.string "name"
+    t.string "water_type"
   end
 
   create_table "tadpoles", force: :cascade do |t|
     t.string  "name"
     t.string  "color"
     t.integer "frog_id"
+    t.integer "pond_id"
   end
 
 end
